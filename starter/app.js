c765f273ca0a0b800d36a1b316f35ad3a9b04910
Ali Ahmed <alitine94@gmail.com>
2023-06-16 02:50:36 -0400

    added controllers and routes

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,20 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+// middleware
+app.use(express.json());
+
+// router
+app.get("/hello", (req, res) => {
+  res.status(200).json({
+    status: 200,
+    data: "hello",
+  });
+});
+
+app.use("/api/v1/tasks", tasks);
+
+app.listen(3000, () => {
+  console.log("server's on!");
+});
